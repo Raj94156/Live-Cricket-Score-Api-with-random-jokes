2281a898a2d532851d06a8240295a2fba47334d0
Raj Gupta <60316730+Raj94156@users.noreply.github.com>
2022-03-14 12:31:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const button = document.querySelector(".container button");
+const jokeDiv = document.querySelector(".container .joke p");
+
+document.addEventListener("DOMContentLoaded", getJock);
+
+button.addEventListener("click", getJock);
+
+async function getJock() {
+  const jokeData = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const jokeObj = await jokeData.json();
+  jokeDiv.innerHTML = jokeObj.joke;
+  console.log(jokeData);
+}
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
